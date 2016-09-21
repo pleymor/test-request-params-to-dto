63527b71f61e54611aff887f32952884d5fc026a
Adrien Laugueux <adrien.laugueux@3ds.com>
2016-09-21 10:22:33 +0200

    first commit

---
+++ src/main/java/com/example/controller/MyController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.dto.TestDto;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+/**
+ * @author Adrien Laugueux.
+ */
+@RestController
+@RequestMapping(path = {"/api"}, produces = APPLICATION_JSON_VALUE)
+public class MyController {
+
+    // GET: dto NOT populated from query params "?a=azaz"
+    @RequestMapping(method = GET, path = "test")
+    public @ResponseBody
+    String test(TestDto testDto){
+        return testDto.toString(); // null
+    }
+
+    // POST: dto WELL populated from body json {"a"="azaz"}
+    @RequestMapping(method = POST, path = "test")
+    public @ResponseBody String postTest(@RequestBody TestDto testDto){
+        return testDto.toString(); // "azaz"
+    }
+}
---
+++ src/main/java/com/example/dto/TestDto.java
@@ -0,0 +1,18 @@
+package com.example.dto;
+
+/**
+ * @author Adrien Laugueux.
+ */
+public class TestDto {
+
+    String a;
+
+    public String getA() {
+        return a;
+    }
+
+    @Override
+    public String toString() {
+        return a;
+    }
+}

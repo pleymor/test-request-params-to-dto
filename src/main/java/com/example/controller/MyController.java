24c17b6e49a324d26e5e0ae5bb2620dd5a6620dc
Adrien Laugueux <adrien.laugueux@3ds.com>
2016-09-21 10:37:39 +0200

    simplify

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import com.example.dto.TestDto;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -19,16 +18,13 @@ public class MyController {
 
     // GET: dto NOT populated from query params "?a=azaz"
     @RequestMapping(method = GET, path = "test")
-    public @ResponseBody
-    TestDto test(TestDto testDto) {
+    public TestDto test(TestDto testDto) {
         return testDto; // null
     }
 
     // POST: dto WELL populated from body json {"a"="azaz"}
     @RequestMapping(method = POST, path = "test")
-    public
-    @ResponseBody
-    TestDto postTest(@RequestBody TestDto testDto) {
+    public TestDto postTest(@RequestBody TestDto testDto) {
         return testDto; // "azaz"
     }
 }
